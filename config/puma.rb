c339a2bc612e2ec96d76fd497c31611b052f78e2
snowdrop2003 <yukihiroakada@gmail.com>
2023-05-05 07:07:24 +0000

    puma error

--- config/puma.rb
+++ config/puma.rb
@@ -41,3 +41,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "unix:///home/ec2-user/bookers2/tmp/sockets/puma.sock"
